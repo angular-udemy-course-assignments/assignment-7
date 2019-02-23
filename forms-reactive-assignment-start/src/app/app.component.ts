b64b275cee0e43efe208c8088e2de9078bf65544
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-23 19:36:12 +0200

    form created

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectStatuses = ['Stable', 'Critical', 'Finished'];
+  defaultStatus = this.projectStatuses[1];
+  createForm: FormGroup;
+
+  ngOnInit(): void {
+    this.createForm = new FormGroup({
+      'projectName': new FormControl(),
+      'email': new FormControl(),
+      'projectStatus': new FormControl(this.defaultStatus)
+    });
+  }
+
+
+  onCreate() {
+    console.log(this.createForm);
+  }
 }
