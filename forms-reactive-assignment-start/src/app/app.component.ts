f1fabcbb4dcca366104a2e96368f71a7b3fe45d7
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-23 20:30:06 +0200

    add validation

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,45 @@ export class AppComponent implements OnInit {
   projectStatuses = ['Stable', 'Critical', 'Finished'];
   defaultStatus = this.projectStatuses[1];
   createForm: FormGroup;
+  blackListName = 'Test';
 
   ngOnInit(): void {
     this.createForm = new FormGroup({
-      'projectName': new FormControl(),
-      'email': new FormControl(),
+      'projectName': new FormControl(
+        null,
+        [Validators.required],
+        this.forbiddenNamesAsync.bind(this)),
+      'email': new FormControl(
+        null,
+        [Validators.required, Validators.email]),
       'projectStatus': new FormControl(this.defaultStatus)
     });
+
+    this.createForm.statusChanges.subscribe((status) => console.log(status));
   }
 
 
   onCreate() {
-    console.log(this.createForm);
+    console.log(this.createForm.value);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (control.value === this.blackListName) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+
+  forbiddenNamesAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === this.blackListName) {
+          resolve({'nameIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
+    return promise;
   }
 }
